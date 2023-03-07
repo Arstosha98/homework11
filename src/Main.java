b4fe074ee36b8d268857e1a73b526117ada98976
Ars_tosha <arsentev98pochta@mail.ru>
2023-03-07 17:31:07 +0500

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,61 @@ public class Main {
         task1();
         task2();
         task3();
-        task4();
-        task5();
-        task6();
-        task7();
     }
-
     public static void task1() {
         System.out.println("Задание 1");
+        getYear(2021);
+    }
+    public static int getYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — не високосный год");
+        }
+        return year;
     }
 
     public static void task2() {
         System.out.println("Задание 2");
+        calculateOSAndYear(1, 2018);
+    }
+    public static int calculateOSAndYear (int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите обычную версию приложения для Android по ссылке");
+            }
+        }
+        return clientOS;
     }
 
     public static void task3() {
         System.out.println("Задание 3");
+        getTotal(95 );
     }
 
-    public static void task4() {
-        System.out.println("Задание 4");
-    }
-
-    public static void task5() {
-        System.out.println("Задание 5");
-    }
-
-    public static void task6() {
-        System.out.println("Задание 6");
-    }
-
-    public static void task7() {
-        System.out.println("Задание 7");
+    public static int getTotal (int deliveryDistance) {
+        int deliveryDay = 1;
+        if (0 < deliveryDistance && deliveryDistance < 20) {
+        }
+        if (deliveryDistance > 20) {
+            deliveryDay++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDay++;
+        }
+        if (deliveryDistance > 100){
+        System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryDay);
+        }
+        return deliveryDay;
     }
 }
